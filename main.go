64592d6ca18df3f1cc9495190edcac0240c92943
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-08 09:59:23 +0100

    Tests added and initial greeting rules

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,6 +19,28 @@ func getName() string {
 	return scanner.Text()
 }
 
+func truncateLongNames(nameString string) string {
+	if len(nameString) > 20 {
+		return nameString[:20] + "... Wow, that name's too long for me!"
+	}
+	return nameString
+}
+
 func greeting(name string) string {
-	return "Hello, " + name
+	var greetingString string
+	nameString := name
+	spaceIndex := strings.Index(name, " ")
+	if spaceIndex != -1 {
+		nameString = name[:spaceIndex]
+	}
+	nameString = truncateLongNames(nameString)
+	greetingString = "Hello, " + nameString
+	creators := [3]string{"Robert Griesemer", "Rob Pike", "Ken Thompson"}
+	for i := 0; i < len(creators); i++ {
+		if name == creators[i] {
+			greetingString = greetingString + ". Thanks for creating me!"
+			break
+		}
+	}
+	return greetingString
 }
--- main_test.go
+++ main_test.go
@@ -2,11 +2,19 @@ package main
 
 import "testing"
 
+var tests = []struct {
+	name     string
+	expected string
+}{
+	{"Alice", "Hello, Alice"},
+	{"Rob Pike", "Hello, Rob. Thanks for creating me!"},
+}
+
 func TestGreeting(t *testing.T) {
-	var result, expected string
-	result = greeting()
-	expected = "Hello, world."
-	if result != expected {
-		t.Errorf("incorrect greeting, got: %s expected: %s", result, expected)
+	for _, test := range tests {
+		result := greeting(test.name)
+		if result != test.expected {
+			t.Errorf("Incorrect greeting for name %s, got: %s but expected: %s", test.name, result, test.expected)
+		}
 	}
 }
